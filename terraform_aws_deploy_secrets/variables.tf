ef6141103aeaf38700685f021658c101742078a6
James Penn III <149443367+jp3407@users.noreply.github.com>
2025-01-12 19:46:10 -0500

    Update variables.tf

--- terraform_aws_deploy_secrets/variables.tf
+++ terraform_aws_deploy_secrets/variables.tf
@@ -1,4 +1,17 @@
 variable "region" {
     description = "The AWS region"
-    default     = "us-east-1"
+    default = "us-east-1"
 }
+
+variable "d_secrets" {
+  default = {
+    secret_name = "my-secret-2"
+    secret_arn = "arn:aws:secretsmanager:us-east-1:381492112805:secret:my-secret-2-o0RNv5"
+  }
+
+  type = map(string)
+}
+
+resource "aws_secretsmanager_secret_version" "my-secret-2" {
+  secret_id     = aws_secretsmanager_secret.my-secret-2.id
+  secret_string = jsonencode(var.d_secrets)
