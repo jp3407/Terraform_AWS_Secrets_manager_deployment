8a1276fbe98191d757619f09ce7fbd1a53c9becd
James Penn III <149443367+jp3407@users.noreply.github.com>
2025-01-12 19:45:02 -0500

    Update provider.tf

--- terraform_aws_deploy_secrets/provider.tf
+++ terraform_aws_deploy_secrets/provider.tf
@@ -1,5 +1,12 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "my_key"
-    secret_key = "secret_key"
+}
+
+resource "aws_secretsmanager_secret" "d_secrets" {
+    name = "my-secret-2"
+}
+
+resource "aws_secretsmanager_secret_version" "my-secret-2" {
+  secret_id     = aws_secretsmanager_secret.my-secret-2.id
+  secret_string = jsonencode(var.d_secrets)
 }
