dde7cb2c132020503c32b56aa9992f232d724117
[James Penn III] <[jamespenniii@gmail.com]>
2025-01-23 16:21:39 -0500

    backend update

--- terraform_aws_deploy_secrets/backend.tf
+++ terraform_aws_deploy_secrets/backend.tf
@@ -1,7 +1,15 @@
 terraform {
     backend "s3" {
       bucket = "my-terraform-state-james2"
-      key    = "state"
+      key    = "s3-github-actions/terraform.tfstate"
       region = "us-east-1"
+      encrypt = true
+    }
+    required_version = ">0.13.0"
+    required_providers {
+      aws = {
+        version = ">=2.7.0"
+        source = "hashicorp/aws"
+      }
     }
 }
